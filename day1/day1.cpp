018ad68fa7b11c26b5cb36fc8f6d9eb6a03b4dcf
Bradley Black <bradley_black@me.com>
2021-12-04 17:33:55 +1300

    Populate integer array with depths

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -19,10 +19,20 @@ bool load_file(std::string file_path, std::vector<std::string> *file_data){
         }
     }catch (const std::ifstream::failure& e){
         std::cout<<"Error reading file: "<<file_path<<"!"<<std::endl;
+        input_file.close();
         return false;
     }
 
+    input_file.close();
+
     return true;
+}
+
+void convert_string_vector_to_array(std::vector<std::string> *string_vector, int *array){
+    for (int i = 0; i < int(string_vector->size()); i++){
+        array[i] = std::stoi(string_vector->at(i), nullptr, 10);
+    }
+
 }
 
 int main(int argc, char *argv[]){
@@ -32,16 +42,16 @@ int main(int argc, char *argv[]){
     }
 
     std::string file_path = argv[1];
-
     std::vector<std::string> file_data;
+    int *depths = nullptr;
 
     if (!load_file(file_path, &file_data)){
         return 0;
     }
 
-    for (int i = 0; i < file_data.size(); i++){
-        std::cout<<file_data[i]<<std::endl;
-    }
+    // Set the depths array to be the same size as the file data
+    depths = new int[file_data.size()];
+    convert_string_vector_to_array(&file_data, depths);
 
     return 1;
 }
