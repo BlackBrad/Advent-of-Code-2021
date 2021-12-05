b277fd8f8287048962663bf09510ef1194dafed9
Bradley Black <bradley_black@me.com>
2021-12-05 15:54:10 +1300

    Add BingoBoard class

---
+++ day4/day4.cpp
@@ -0,0 +1,72 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+typedef struct BingoBoardEntry{
+    int entry;
+    bool marked;
+} BingoBoardEntry_t;
+
+class BingoBoard{
+    private:
+        BingoBoardEntry_t **board_array = nullptr;
+    public:
+        BingoBoard();
+        ~BingoBoard();
+
+        BingoBoardEntry_t get_board_entry(int x, int y){
+            return board_array[x][y];
+        }
+};
+
+bool load_file(std::string file_path, std::vector<std::string> *file_data){
+    std::string temp_line;
+    std::ifstream input_file;
+    input_file.exceptions (std::ifstream::badbit ); // No need to check failbit
+
+    try{
+        input_file.open(file_path);
+        if (!input_file.is_open()){
+            std::cout<<"Could not open file: "<<file_path<<std::endl;
+            return false;
+        }
+        while(std::getline(input_file, temp_line)){
+            file_data->push_back(temp_line);
+        }
+    }catch (const std::ifstream::failure& e){
+        std::cout<<"Error reading file: "<<file_path<<"!"<<std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char *argv[]){
+    /*
+     * TEMPLATE
+     */
+    if (argc < 2){
+        std::cout<<"Usage: ./puzzle FILE_PATH"<<std::endl;
+        return 1;
+    }
+
+    std::string file_path = argv[1];
+
+    std::vector<std::string> file_data;
+
+    if (!load_file(file_path, &file_data)){
+        return 1;
+    }
+
+    /*
+     * PART ONE
+     */
+    //part one goes here
+
+    /*
+     * PART TWO
+     */
+    //part two goes here
+    return 0;
+}
